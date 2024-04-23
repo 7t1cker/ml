5fa5c0a5702cc54f9d9f14f9637b0e3114f73ebc
7t1cker <116420305+7t1cker@users.noreply.github.com>
2024-04-23 14:47:10 +0300

    2
    2

---
+++ linear regressions/lr5.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+class MyLineReg():
+    def __init__(self, n_iter=100, learning_rate=0.1, weights=None, metric=None, reg=None, l1_coef=0, l2_coef=0):
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+        self.weights = weights
+        self.metric = metric
+        self.reg = reg
+        self.l1_coef = l1_coef
+        self.l2_coef = l2_coef
+        self.best_score = None
+        self.final_predictions = None
+
+    def fit(self, X, y, verbose=False):
+        X = pd.concat([pd.Series(1, index=X.index, name='bias'), X], axis=1)
+        n_samples, n_features = X.shape
+        
+        if self.weights is None:
+            self.weights = np.ones(n_features)
+        
+        for i in range(self.n_iter):
+
+            y_pred = X.dot(self.weights)
+            error = y_pred - y
+            
+            gradient = (2 / n_samples) * X.T.dot(error)
+            
+            if self.reg == 'l1':
+                gradient[1:] += self.l1_coef * np.sign(self.weights[1:])
+            elif self.reg == 'l2':
+                gradient[1:] += 2 * self.l2_coef * self.weights[1:]
+            elif self.reg == 'elasticnet':
+                gradient[1:] += self.l1_coef * np.sign(self.weights[1:]) + 2 * self.l2_coef * self.weights[1:]
+            
+            self.weights -= self.learning_rate * gradient
+
+            if self.metric is not None:
+                self.final_predictions = X.dot(self.weights)
+                metric_value = self.calculate_metric(y, self.final_predictions)
+                if verbose:
+                    print(f"{i + 1} | {self.metric}: {metric_value:.10f}")
+                self.best_score = metric_value
+
+    def get_coef(self):
+        return self.weights[1:]
+
+    def predict(self, X):
+        X = pd.concat([pd.Series(1, index=X.index, name='bias'), X], axis=1)
+        return X.dot(self.weights)
+
+    def calculate_metric(self, y_true, y_pred):
+        if self.metric == 'mse':
+            return mean_squared_error(y_true, y_pred)
+        else:
+            raise ValueError("Unsupported metric!")
+
+    def get_best_score(self):
+        return self.best_score
